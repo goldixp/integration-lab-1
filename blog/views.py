fce226e1461bbf423dba277186d150194d5dbb88
Anna Godlewska <godlewska.anna04@gmail.com>
2026-03-12 09:47:07 +0100

    Fixing issues

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+def post_list(request):
+    posts = Post.objects.all().order_by('-published_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
